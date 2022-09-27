064868a46f48322be3045e3db8fe37c770ede90d
Shirajuki <shirajuki00@gmail.com>
2022-09-27 02:43:22 +0200

    fix: add local file upload provider fix

--- server/providers/uploads/local/index.ts
+++ server/providers/uploads/local/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Provider } from '../../../uploads/provider'
 import process from 'process'
 import path from 'path'
@@ -7,19 +8,20 @@ import config from '../../../config/server'
 import { FastifyInstance } from 'fastify'
 import fastifyStatic from 'fastify-static'
 import contentDisposition from 'content-disposition'
+import { getAllChallenges } from '../../../database/challenges'
 
 interface LocalProviderOptions {
-  uploadDirectory?: string;
-  endpoint?: string;
+  uploadDirectory?: string
+  endpoint?: string
 }
 
 interface Upload {
-  filePath: string;
-  name: string;
+  filePath: string
+  name: string
 }
 
 interface RequestQuerystring {
-  key: string;
+  key: string
 }
 
 export default class LocalProvider implements Provider {
@@ -28,7 +30,7 @@ export default class LocalProvider implements Provider {
 
   private uploadMap: Map<string, Upload>
 
-  constructor (options: LocalProviderOptions, app: FastifyInstance) {
+  constructor(options: LocalProviderOptions, app: FastifyInstance) {
     if (options.uploadDirectory === undefined) {
       options.uploadDirectory = path.join(process.cwd(), 'uploads')
     }
@@ -40,62 +42,95 @@ export default class LocalProvider implements Provider {
 
     this.uploadMap = new Map<string, Upload>()
 
-    void app.register(async (fastify) => {
-      void fastify.register(fastifyStatic, {
-        root: this.uploadDirectory,
-        serve: false
-      })
+    // Load upload files if exist
+    void this.load()
 
-      // Fastify bug #2466
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
-      fastify.setNotFoundHandler(async (req, res) => {
-        void res.status(404)
-        return 'Not found'
-      })
+    void app.register(
+      async (fastify) => {
+        void fastify.register(fastifyStatic, {
+          root: this.uploadDirectory,
+          serve: false
+        })
 
-      fastify.get<{
-        Querystring: RequestQuerystring
-      }>('/', {
-        schema: {
-          querystring: {
-            type: 'object',
-            properties: {
-              key: {
-                type: 'string'
+        // Fastify bug #2466
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        fastify.setNotFoundHandler(async (req, res) => {
+          void res.status(404)
+          return 'Not found'
+        })
+
+        fastify.get<{
+          Querystring: RequestQuerystring
+        }>(
+          '/',
+          {
+            schema: {
+              querystring: {
+                type: 'object',
+                properties: {
+                  key: {
+                    type: 'string'
+                  }
+                },
+                required: ['key']
               }
-            },
-            required: ['key']
-          }
-        }
-      }, async (request, reply) => {
-        const key = request.query.key.toString()
+            }
+          },
+          async (request, reply) => {
+            const key = request.query.key.toString()
 
-        const upload = this.uploadMap.get(key)
-        if (upload != null) {
-          void reply.header('Cache-Control', 'public, max-age=31557600, immutable')
-          void reply.header('Content-Disposition', contentDisposition(upload.name))
-          void reply.sendFile(path.relative(this.uploadDirectory, upload.filePath))
-        } else {
-          reply.callNotFound()
-        }
-      })
-    }, {
-      prefix: this.endpoint
-    })
+            const upload = this.uploadMap.get(key)
+            if (upload != null) {
+              void reply.header('Cache-Control', 'public, max-age=31557600, immutable')
+              void reply.header('Content-Disposition', contentDisposition(upload.name))
+              void reply.sendFile(path.relative(this.uploadDirectory, upload.filePath))
+            } else {
+              reply.callNotFound()
+            }
+          }
+        )
+      },
+      {
+        prefix: this.endpoint
+      }
+    )
   }
 
-  private getKey (hash: string, name: string): string {
+  async load(): Promise<void> {
+    const disk = fs.readdirSync(this.uploadDirectory)
+    console.log('Found files in upload dir:', disk)
+    if (disk?.length > 0) {
+      const challs = await getAllChallenges()
+      console.log('Found challs in database:', challs.length)
+      challs.forEach((chall) => {
+        console.log('Chall has files:', chall.data.name, chall.data.files.length)
+        chall.data.files.forEach((file) => {
+          // eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
+          const key = (file.url.match('key=[a-fA-F0-9]{64}') || '=')[0].split('=')[1]
+          if (disk.includes(key)) {
+            console.log('Found key in url:', key)
+            const filePath = path.join(this.uploadDirectory, key)
+            this.uploadMap.set(key, {
+              filePath,
+              name: file.name
+            })
+          }
+        })
+      })
+    }
+    console.log('New uploadMap:', this.uploadMap)
+  }
+
+  private getKey(hash: string, name: string): string {
     return `${hash}/${name}`
   }
 
-  private getUrlPath (key: string): string {
+  private getUrlPath(key: string): string {
     return `${this.endpoint}?key=${encodeURIComponent(key)}`
   }
 
-  async upload (data: Buffer, name: string): Promise<string> {
-    const hash = crypto.createHash('sha256')
-      .update(data)
-      .digest('hex')
+  async upload(data: Buffer, name: string): Promise<string> {
+    const hash = crypto.createHash('sha256').update(data).digest('hex')
 
     const key = this.getKey(hash, name)
     const urlPath = this.getUrlPath(key)
@@ -111,7 +146,7 @@ export default class LocalProvider implements Provider {
     return (config.origin || '') + urlPath
   }
 
-  async getUrl (sha256: string, name: string): Promise<string|null> {
+  async getUrl(sha256: string, name: string): Promise<string | null> {
     const key = this.getKey(sha256, name)
     if (!this.uploadMap.has(key)) return null
 
